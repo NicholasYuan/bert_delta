df9a699e0929889d59fd15abc43b318d5c4aa7ec
Nicholas <m332463970@163.com>
2020-03-21 21:46:46 +0800

    add python path

--- BERT-SQuAD/training/run_squad.py
+++ BERT-SQuAD/training/run_squad.py
@@ -23,6 +23,10 @@ import os
 import random
 import glob
 
+import sys
+sys.path.append('../../transformers/src/')
+
+
 import numpy as np
 import torch
 from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler,
