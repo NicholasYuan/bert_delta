356798268c7158490ad0ca29aa41bad01751095a
Nicholas <m332463970@163.com>
2020-03-22 15:56:45 +0800

    add eval log at every step

--- BERT-SQuAD/training/run_squad_delta.py
+++ BERT-SQuAD/training/run_squad_delta.py
@@ -649,6 +649,8 @@ def main():
 
             result = dict((k + ('_{}'.format(global_step) if global_step else ''), v) for k, v in result.items())
             results.update(result)
+            
+            logger.info("Results: {}".format(results))
 
     logger.info("Results: {}".format(results))
 
