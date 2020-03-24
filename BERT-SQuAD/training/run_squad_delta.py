4f1117077022c3ae66a82ec35d1a5dc6b5d1bcd9
Nicholas <m332463970@163.com>
2020-03-24 16:32:44 +0800

    fix import

--- BERT-SQuAD/training/run_squad_delta.py
+++ BERT-SQuAD/training/run_squad_delta.py
@@ -55,7 +55,7 @@ from utils_squad import (read_squad_examples, convert_examples_to_features,
 # You can remove it from the dependencies if you are using this script outside of the library
 # We've added it here for automated tests (see examples/test_examples.py file)
 from utils_squad_evaluate import EVAL_OPTS, main as evaluate_on_squad
-from eval_squad import evaluate_adversarial
+from ..eval_squad import evaluate_adversarial
 
 logger = logging.getLogger(__name__)
 
